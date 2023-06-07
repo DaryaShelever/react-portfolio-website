91d9069aa2cbec503caf22aba87559b07bd5a964
Darya Shelever <d.shelever@gmail.com>
2023-06-07 19:20:46 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Footer.css'
+import{BsLinkedin} from 'react-icons/bs'
+import{FaGithub} from 'react-icons/fa'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>Darya Shelever</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/darya-shelever/"><BsLinkedin/></a>
+        <a href="https://github.com/DaryaShelever" target="_blank" ><FaGithub/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
