bd6b4e29303c01e1f21f6210a81e4c90e1ad32c9
Darya Shelever <d.shelever@gmail.com>
2023-06-07 17:57:29 +0300

    contact_container

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className='container contact_container'>
         <div className="contact_options">
           <article className='contact_option'>
-            <MdOutlineMailOutline/>
+            <MdOutlineMailOutline className='contact_option-icon'/>
             <h4>Email</h4>
             <h5>d.shelever@gmail.com</h5>
             <a href="mailto:d.shelever@gmail.com" target='_blank'>Send a message</a>
@@ -20,12 +20,18 @@ const Contact = () => {
 
           
           <article className='contact_option'>
-            <AiOutlineWhatsApp/>
+            <AiOutlineWhatsApp className='contact_option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+972586008587</h5>
-            <a href="https://api.whatsapp.com/send?phone=+972586008587" target='_blank'>Send a message</a>
+            <a href="https://wa.me/+972586008587" target='_blank'>Send a message</a>
           </article>
         </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name ' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="massage"   rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
 
     </section>
